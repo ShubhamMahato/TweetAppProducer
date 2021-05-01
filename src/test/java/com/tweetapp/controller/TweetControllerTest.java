6b74f14e4682e59298aa503fd79ad83d8e6975ae
ShubhamMahato <Shubham6852@gmail.com>
2021-05-01 17:15:24 +0530

    sdsd

---
+++ src/test/java/com/tweetapp/controller/TweetControllerTest.java
@@ -0,0 +1,140 @@
+package com.tweetapp.controller;
+
+import com.tweetapp.model.Tweet;
+import com.tweetapp.model.TweetEventType;
+import com.tweetapp.model.response.UserLoginResponse;
+import com.tweetapp.service.TweetService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collections;
+import java.util.Date;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TweetControllerTest {
+
+    @InjectMocks TweetController tweetController;
+
+    @Mock
+    TweetService tweetService;
+
+    @Test
+    public void postTweet(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").message("sdafdsfsdfs").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").build();
+
+        Mockito.when(tweetService.postTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.postTweet(tweet,username);
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void postTweet_NullTweet(){
+        Tweet tweet=null;
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").build();
+
+        Mockito.when(tweetService.postTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.postTweet(tweet,username);
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void postTweet_Error(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").message("sdafdsfsdfs").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.postTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.postTweet(tweet,username);
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void updateTweet(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").message("sdafdsfsdfs").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.updateTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.updateTweet(tweet,username,"dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+    @Test
+    public void updateTweet_blankUsername(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").message("sdafdsfsdfs").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.updateTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.updateTweet(tweet,null,"dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void updateTweet_NullMessage(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.updateTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.updateTweet(tweet,null,"dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void deleteTweet(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.deleteTweet(Mockito.any(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.deleteTweet(null,"dwdwwd");
+        Assert.assertNotNull(res.getBody());
+
+    }
+
+    @Test
+    public void replyTweet(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        String username="dadadada";
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.replyTweet(Mockito.any(),Mockito.anyString(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.replyTweet(tweet,null,"dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+
+
+    @Test
+    public void likeTweet(){
+        UserLoginResponse userLoginResponse= UserLoginResponse.builder().username("adss").email("dadadasdas").lastName("dsdsd").firstName("dadad").error("sdsdsdsd").build();
+
+        Mockito.when(tweetService.likeTweet(Mockito.anyString(),Mockito.anyString())).thenReturn(userLoginResponse);
+        ResponseEntity res=tweetController.likeTweet("kjhjl","dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void getAllTweets(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        Mockito.when(tweetService.getAllTweets(Mockito.anyString())).thenReturn(Collections.singletonList(tweet));
+        ResponseEntity res=tweetController.getAllTweets("dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+
+    @Test
+    public void getAllUsersTweets(){
+        Tweet tweet=Tweet.builder().id("dsdsdsds").tweetEventType(TweetEventType.NEW).postDate(new Date()).build();
+        Mockito.when(tweetService.getAllUsersTweets(Mockito.anyString())).thenReturn(Collections.singletonList(tweet));
+        ResponseEntity res=tweetController.getAllUsersTweets("dwdwwd");
+        Assert.assertNotNull(res.getBody());
+    }
+}
